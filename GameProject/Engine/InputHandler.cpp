3ec544001ab2a9e407dcd184a14674a137b5ce0c
SIMON NYLÉN <sina16@student.bth.se>
2019-01-29 16:45:38 +0100

    Added support for mouse movement and clicking

--- GameProject/Engine/InputHandler.cpp
+++ GameProject/Engine/InputHandler.cpp
@@ -11,18 +11,21 @@ void InputHandler::keyCallback(GLFWwindow * window, int key, int scancode, int a
 void InputHandler::mouseMoveCallback(GLFWwindow * window, double xpos, double ypos)
 {
 	//Send event when mouse is moved to EventBus
+	//EventBus::get().publish(new _EVENT_TYPE_());
 }
 
 //action = [GLFW_PRESS, GLFW_RELEASE]
 void InputHandler::mouseClickCallback(GLFWwindow * window, int button, int action, int mods)
 {
 	//Send event when mouse is clicked to EventBus
+	//EventBus::get().publish(new _EVENT_TYPE_());
 }
 
 InputHandler::InputHandler(GLFWwindow* window)
 {
 	glfwSetKeyCallback(window, this->keyCallback);
-
+	//glfwSetCursorPosCallback(window, this->mouseMoveCallback);
+	//glfwSetMouseButtonCallback(window, this->mouseClickCallback);
 }
 
 
