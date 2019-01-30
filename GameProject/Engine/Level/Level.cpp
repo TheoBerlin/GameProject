9460bc7a62b17abbc7708120413b8a2d6ec0abe5
Anton Åsbrink <anton.asbrink@hotmail.com>
2019-01-30 12:28:41 +0100

    Fixed naming scheme and changed to pragma

--- GameProject/Engine/Level/Level.cpp
+++ GameProject/Engine/Level/Level.cpp
@@ -2,33 +2,33 @@
 
 void Level::writePosition(int index, glm::vec3 position)
 {
-	j[index]["X"] = position.x;
-	j[index]["Y"] = position.y;
-	j[index]["Z"] = position.z;
+	jsonFile[index]["X"] = position.x;
+	jsonFile[index]["Y"] = position.y;
+	jsonFile[index]["Z"] = position.z;
 }
 
 void Level::writeName(int index, std::string name)
 {
-	j[index]["Name"] = name;
+	jsonFile[index]["Name"] = name;
 }
 
 
 void Level::writeToFile(std::string file, glm::vec3 position, glm::vec3 scale)
 {
 	std::ofstream oStream(file);
-	oStream << std::setw(4) << j << std::endl;
+	oStream << std::setw(4) << jsonFile << std::endl;
 }
 
 
 std::string Level::read(std::string file)
 {
 	std::ifstream i(file);
-	i >> j;
+	i >> jsonFile;
 	std::string string = "";
 
-	for (int i = 0; i < j.size(); i++) {
+	for (int i = 0; i < jsonFile.size(); i++) {
 		string += "Index " + std::to_string(i) + ":\n";
-		string += j[i]["Name"].dump();
+		string += jsonFile[i]["Name"].dump();
 		string += "\n";
 	}
 	
