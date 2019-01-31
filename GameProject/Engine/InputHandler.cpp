0344591ba5998c29821e8d8f5263a92c391decd0
SIMON NYLÉN <sina16@student.bth.se>
2019-01-31 11:52:35 +0100

    Removed memory leaks

--- GameProject/Engine/InputHandler.cpp
+++ GameProject/Engine/InputHandler.cpp
@@ -5,20 +5,20 @@
 //action = [GLFW_PRESS, GLFW_REPEAT, GLFW_RELEASE]
 void InputHandler::keyCallback(GLFWwindow * window, int key, int scancode, int action, int mods)
 {
-	EventBus::get().publish(new KeyEvent(key, action));
+	EventBus::get().publish(&KeyEvent(key, action));
 }
 
 void InputHandler::mouseMoveCallback(GLFWwindow * window, double xpos, double ypos)
 {
 	//Send event when mouse is moved to EventBus
-	//EventBus::get().publish(new _EVENT_TYPE_());
+	//EventBus::get().publish(&_EVENT_TYPE_());
 }
 
 //action = [GLFW_PRESS, GLFW_RELEASE]
 void InputHandler::mouseClickCallback(GLFWwindow * window, int button, int action, int mods)
 {
 	//Send event when mouse is clicked to EventBus
-	//EventBus::get().publish(new _EVENT_TYPE_());
+	//EventBus::get().publish(&_EVENT_TYPE_());
 }
 
 InputHandler::InputHandler(GLFWwindow* window)
