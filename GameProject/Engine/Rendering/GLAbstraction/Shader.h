8d06a2eea25945179bfa394442a1ad56cd10ac5d
Jonathan Åleskog <jonathan9609082000@gmail.com>
2019-01-31 11:46:19 +0100

    Added more uniform functions in Shader

--- GameProject/Engine/Rendering/GLAbstraction/Shader.h
+++ GameProject/Engine/Rendering/GLAbstraction/Shader.h
@@ -18,7 +18,223 @@ public:
 	void unbind();
 
 	const GLuint getID() const;
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform1b(const std::string& name, bool value);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform1i(const std::string& name, int value);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform2i(const std::string& name, int v1, int v2);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform2iv(const std::string& name, unsigned int count, const int* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform3i(const std::string& name, int v1, int v2, int v3);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform3iv(const std::string& name, unsigned int count, const int* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform4i(const std::string& name, int v1, int v2, int v3, int v4);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform4iv(const std::string& name, unsigned int count, const int* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform1ui(const std::string& name, unsigned int value);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform2ui(const std::string& name, unsigned int v1, unsigned int v2);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform2uiv(const std::string& name, unsigned int count, const unsigned int* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform3ui(const std::string& name, int v1, int v2, int v3);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform3uiv(const std::string& name, unsigned int count, const unsigned int* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform4ui(const std::string& name, int v1, int v2, int v3, int v4);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform4uiv(const std::string& name, unsigned int count, const unsigned int* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
 	void setUniform1f(const std::string& name, float f);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform2f(const std::string& name, float v1, float v2);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform2fv(const std::string& name, unsigned int count, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform3f(const std::string& name, float v1, float v2, float v3);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform3fv(const std::string& name, unsigned int count, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform4f(const std::string& name, float v1, float v2, float v3, float v4);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniform4fv(const std::string& name, unsigned int count, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix2fv(const std::string& name, unsigned int count, bool transpose, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix3fv(const std::string& name, unsigned int count, bool transpose, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix4fv(const std::string& name, unsigned int count, bool transpose, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix2x3fv(const std::string& name, unsigned int count, bool transpose, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix3x2fv(const std::string& name, unsigned int count, bool transpose, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix2x4fv(const std::string& name, unsigned int count, bool transpose, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix4x2fv(const std::string& name, unsigned int count, bool transpose, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix3x4fv(const std::string& name, unsigned int count, bool transpose, const float* values);
+
+	/*
+	Add uniform if not found, then set it to the new value.
+	Arguments:
+		name: Name of the uniform in the shader.
+	*/
+	void setUniformMatrix4x3fv(const std::string& name, unsigned int count, bool transpose, const float* values);
 
 private:
 	void init(const std::string& vertex, const std::string& fragment);
