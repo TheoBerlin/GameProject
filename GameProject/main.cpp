d52a4433a7065ad4977af9278cfa9708c4ecda91
Anton Åsbrink <anton.asbrink@hotmail.com>
2019-02-01 14:34:15 +0100

    Started OpenAL
    Added openAL but require more testing

--- GameProject/main.cpp
+++ GameProject/main.cpp
@@ -4,15 +4,32 @@
 #include "Game/Game.h"
 #include "Engine/AssetManagement/ModelLoader.h"
 
+#include "OpenAL/al.h"
+#include "OpenAL/alc.h"
+
 #include "ft2build.h" //DO NOT MOVE HEADER FILE FROM ROOT DIRECTORY OF INCLUDE
 #include FT_FREETYPE_H  
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "../../Utils/stb_image.h" //Single library for img loader
 
+#define NUM_BUFFERS 3
+#define BUFFER_SIZE 4096
+
 int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	Logger::init();
+
+	alGetError();
+
+	ALCdevice * deviceAL = alcOpenDevice(NULL);
+
+	if (deviceAL == NULL)
+	{
+		LOG_ERROR("Failed to init OpenAL device.");
+	}
+
+	ALCcontext * contextAL = alcCreateContext(deviceAL, NULL);
 
 	Game game;
 	game.start();
