648a37c5ca6b1c9291654570ccf2b9d4a9eccdc7
SIMON NYLÉN <sina16@student.bth.se>
2019-02-05 14:31:31 +0100

    Fixed FreeMove with mouse and keyboard.

--- GameProject/Engine/Components/FreeMove.cpp
+++ GameProject/Engine/Components/FreeMove.cpp
@@ -12,7 +12,13 @@ FreeMove::FreeMove(const std::string& tagName) : Component(tagName)
 	EventBus::get().subscribe(this, &FreeMove::moveMouse);
 	EventBus::get().subscribe(this, &FreeMove::clickMouse);
 
-	this->active = false;
+	this->speed = 100.0f;
+	this->sensitivity = 10.0f;
+	this->mouseLock = false;
+	this->xPos = 0.0;
+	this->yPos = 0.0;
+	this->preXPos = 0.0;
+	this->preYPos = 0.0;
 }
 
 
@@ -29,86 +35,74 @@ void FreeMove::init()
 
 void FreeMove::update(const float & dt)
 {
+	EntityMatrix * mat = getHost()->getMatrix();
 	this->dt = dt;
+
+	// Keys that need constant polling
+	if (this->pressedKeys[GLFW_KEY_W])
+		mat->setPosition(mat->getPosition() + this->dt * mat->getForward() * this->speed);
+	if (this->pressedKeys[GLFW_KEY_A])
+		mat->setPosition(mat->getPosition() + this->dt * -mat->getRight() * this->speed);
+	if (this->pressedKeys[GLFW_KEY_D])
+		mat->setPosition(mat->getPosition() + this->dt * mat->getRight() * this->speed);
+	if (this->pressedKeys[GLFW_KEY_S])
+		mat->setPosition(mat->getPosition() + this->dt * -mat->getForward() * this->speed);
+	if (this->pressedKeys[GLFW_KEY_SPACE])
+		mat->setPosition(mat->getPosition() + this->dt * mat->getUp() * this->speed);
+	if (this->pressedKeys[GLFW_KEY_LEFT_CONTROL])
+		mat->setPosition(mat->getPosition() + this->dt * -mat->getUp() * this->speed);
+
+	if (this->mouseLock)
+	{
+		if (this->xPos != 0.0 || this->yPos != 0.0)
+		{
+			EntityMatrix * mat = getHost()->getMatrix();
+			glm::vec3 oldForward = mat->getForward();
+
+			oldForward = glm::rotate(oldForward, -(float)xPos * this->dt * this->sensitivity, mat->getUp());
+			oldForward = glm::rotate(oldForward, -(float)yPos * this->dt * this->sensitivity, mat->getRight());
+
+			this->xPos = 0.0;
+			this->yPos = 0.0;
+
+			getHost()->getMatrix()->setForward(oldForward);
+		}
+	}
 }
 
 void FreeMove::moveKeyboard(KeyEvent * evnt)
 {
-	EntityMatrix * mat = getHost()->getMatrix();
-	//if (evnt->action == GLFW_PRESS)
-	//{
-	static const float speed = 100.0f;
-	
-	switch (evnt->key)
+	if (evnt->action == GLFW_PRESS)
+		this->pressedKeys[evnt->key] = true;
+	else if (evnt->action == GLFW_RELEASE)
+		this->pressedKeys[evnt->key] = false;
+
+	// Toggle keys
+	if (evnt->key == GLFW_KEY_C && evnt->action == GLFW_PRESS)
 	{
-	case GLFW_KEY_W:
-		mat->setPosition(mat->getPosition() + this->dt * mat->getForward() * speed);
-		break;
-	case GLFW_KEY_A:
-		mat->setPosition(mat->getPosition() + this->dt * -mat->getRight() * speed);
-		break;
-	case GLFW_KEY_D:
-		mat->setPosition(mat->getPosition() + this->dt * mat->getRight() * speed);
-		break;
-	case GLFW_KEY_S:
-		mat->setPosition(mat->getPosition() + this->dt * -mat->getForward() * speed);
-		break;
-	case GLFW_KEY_SPACE:
-		mat->setPosition(mat->getPosition() + this->dt * mat->getUp() * speed);
-		break;
-	case GLFW_KEY_LEFT_CONTROL:
-		mat->setPosition(mat->getPosition() + this->dt * -mat->getUp() * speed);
-		break;
-	case GLFW_KEY_LEFT:
-		{
-		glm::vec3 tempForward = mat->getForward();
-		tempForward = glm::rotate(tempForward, this->dt*speed, mat->getUp());
-		mat->setForward(tempForward);
-		}
-		break;
-	case GLFW_KEY_RIGHT:
-	{
-		glm::vec3 tempForward = mat->getForward();
-		tempForward = glm::rotate(tempForward, -this->dt*speed, mat->getUp());
-		mat->setForward(tempForward);
+		this->mouseLock = !this->mouseLock;
+		//glfwSetCursorPos(Display::get().getWindowPtr(), Display::get().getWidth() / 2, Display::get().getHeight() / 2);
+		
+		if (this->mouseLock)
+			glfwSetInputMode(Display::get().getWindowPtr(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		else
+			glfwSetInputMode(Display::get().getWindowPtr(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 	}
-		break;
-	case GLFW_KEY_UP:
-	{
-		glm::vec3 tempForward = mat->getForward();
-		tempForward = glm::rotate(tempForward, this->dt*speed, mat->getRight());
-		mat->setForward(tempForward);
-	}
-		break;
-	case GLFW_KEY_DOWN:
-	{
-		glm::vec3 tempForward = mat->getForward();
-		tempForward = glm::rotate(tempForward, -this->dt*speed, mat->getRight());
-		mat->setForward(tempForward);
-	}
-		break;
-	}
-	
-	//}
 }
 
 void FreeMove::moveMouse(MouseMoveEvent * evnt)
 {
-	/*
-	EntityMatrix * mat = getHost()->getMatrix();
-	glm::vec3 tempForward = mat->getForward();
+	this->xPos = evnt->xpos - this->preXPos;
+	this->yPos = evnt->ypos - this->preYPos;
+	this->preXPos = evnt->xpos;
+	this->preYPos = evnt->ypos;
 
-	tempForward = glm::rotate(tempForward, this->dt*(float)evnt->xpos, mat->getUp());
-	tempForward = glm::rotate(tempForward, this->dt*(float)evnt->ypos, mat->getRight());
+	//this->xPos -= Display::get().getWidth() / 2;
+	//this->yPos -= Display::get().getHeight() / 2;
 
-	mat->setForward(tempForward);*/
+	LOG_PRINT("XPOS: %f, YPOS: %f", this->xPos, this->yPos);
 }
 
 void FreeMove::clickMouse(MouseClickEvent * evnt)
 {
-	if (evnt->action == GLFW_PRESS)
-	{
-		EntityMatrix * mat = getHost()->getMatrix();
-		this->active = !this->active;
-	}
 }
--- GameProject/Engine/Components/FreeMove.h
+++ GameProject/Engine/Components/FreeMove.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Component.h"
 #include "../Events/Events.h"
+#include <unordered_map>
 
 class FreeMove : public Component
 {
@@ -13,8 +14,12 @@ public:
 
 private:
 	float dt;
-	bool active;
 	float speed;
+	float sensitivity;
+	bool mouseLock;
+	double xPos, yPos;
+	double preXPos, preYPos;
+	std::unordered_map<unsigned, bool> pressedKeys;
 
 	void moveKeyboard(KeyEvent * evnt);
 	void moveMouse(MouseMoveEvent * evnt);
