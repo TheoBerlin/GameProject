ad91a0a21815f5458abf71fcb01cb956a646176c
ADRIAN NORDIN <adno16@student.bth.se>
2019-02-12 10:35:48 +0100

    Added two diffrent phases, that can be swapped between with keys 1, 2

--- GameProject/Game/GameLogic.h
+++ GameProject/Game/GameLogic.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Engine/Entity/EntityManager.h"
+#include "glm/glm.hpp"
+#include "Engine/Events/Events.h"
 
 class GameLogic
 {
@@ -8,10 +10,19 @@ public:
 	GameLogic(EntityManager * em);
 	~GameLogic();
 
-	void changePhase(unsigned phase);
+	enum Phases { PHASE_ONE, PHASE_TWO, PHASE_THREE };
+	void changePhase(Phases phase);
+
+	void enterPhaseOne(const glm::vec3 & cameraPos, const glm::vec3 & cameraDir);
+	void enterPhaseTwo(const glm::vec3 & playerPos);
+
+	void leavePhaseOne();
+	void leavePhaseTwo();
 
 private:
-	unsigned currentPhase;
+	void changePhaseCallback(KeyEvent * ev);
+
+	Phases currentPhase;
 
 	Entity* camera;
 	Entity* player;
