146457a47b9a852e5e1706b24499c8541416aeb7
SIMON NYLÉN <sina16@student.bth.se>
2019-02-13 13:15:24 +0100

    Now recognizes collisions and reports them.

--- GameProject/Engine/Collision/CollisionHandler.cpp
+++ GameProject/Engine/Collision/CollisionHandler.cpp
@@ -1,13 +1,111 @@
 #include "CollisionHandler.h"
+#include "Engine/Entity/Entity.h"
+#include "Utils/Logger.h"
 
 
 CollisionHandler::CollisionHandler()
 {
-	this->test = new rp3d::CollisionWorld();
+	// Set default values
+	this->takenBodies = 0;
+
+	// Set settings for world
+	rp3d::WorldSettings settings;
+	settings.defaultVelocitySolverNbIterations = 20;
+
+	// Create the world with the settings
+	this->world = new rp3d::CollisionWorld(settings);
 }
 
 
 CollisionHandler::~CollisionHandler()
 {
-	delete test;
+	delete this->world;
+
+	//for (size_t i = 0; i < this->bodies.size(); i++)
+	//{
+	//	removeCollisionBody(this->bodies[this->bodies.size()]);
+	//}
+}
+
+void CollisionHandler::update(const float & dt)
+{
+	this->world->testCollision(&this->collision);
+	
+	//for (int i = 0; i < this->bodies.size(); i++)
+	//	LOG_INFO("POSITIONS: (%f, %f)", this->bodies[i]->getTransform().getPosition().x, this->bodies[i]->getTransform().getPosition().y);
+}
+
+void CollisionHandler::createCollisionBodies(int num)
+{
+	size_t previousSize = this->bodies.size();
+	this->bodies.resize(this->bodies.size() + num);
+
+	// Standard values for bodies which is not in use
+	rp3d::Vector3 initPosition(0.0, 0.0, 0.0);
+	rp3d::Quaternion initOrientation = rp3d::Quaternion::identity();
+	rp3d::Transform transform(initPosition, initOrientation);
+
+	// Create the bodies in the world and add them to the map
+	for (size_t i = previousSize; i < num; i++)
+	{
+		this->bodies[i] = this->world->createCollisionBody(transform);
+	}
+}
+
+rp3d::CollisionBody * CollisionHandler::getUnusedBody()
+{
+	if (this->takenBodies == this->bodies.size())
+		return nullptr;
+	else
+		return this->bodies[this->takenBodies++];
+}
+
+void CollisionHandler::addCollisionToEntity(Entity * entity, Shape shape)
+{
+	rp3d::CollisionBody* entityBody = getUnusedBody();
+
+	rp3d::Vector3 pos = toReactVec(entity->getTransform()->getPosition());
+	rp3d::Quaternion rotation = rp3d::Quaternion::identity(); //<--------CHANGE THIS TO ENTITY ROTATION WHEN WE HAVE QUATERNIONS
+	rp3d::Transform transform(pos, rotation);
+
+	/*
+	SET SHAPE TO COLLISIONBODY. IF SHAPE IS ARROW/PLAYER SAVE THE POINTER FOR COLLISION CHECK
+	*/
+	switch (shape)
+	{
+	case Shape::ARROW:
+		this->player = entityBody;
+		// SET SHAPE!
+		break;
+	case Shape::BOX:
+	{
+		// Set shapes
+		rp3d::Transform transform = rp3d::Transform::identity();
+		rp3d::ProxyShape* boxShape = entityBody->addCollisionShape(&this->shape, transform);
+		break;
+	}
+	case Shape::DRONE:
+		// SET SHAPE!
+		break;
+	default:
+		LOG_ERROR("Unknown shape");
+	}
+
+	entityBody->setTransform(transform);
+
+	entity->setCollisionBody(entityBody);
+}
+
+void CollisionHandler::removeCollisionBody(rp3d::CollisionBody * body)
+{
+}
+
+rp3d::Vector3 CollisionHandler::toReactVec(const glm::vec3 & vec)
+{
+	return { vec.x, vec.y, vec.z };
+}
+
+glm::vec3 CollisionHandler::toGlmVec(const rp3d::Vector3 & vec)
+{
+	return { vec.x, vec.y, vec.z };
 }
--- GameProject/Engine/Collision/CollisionHandler.h
+++ GameProject/Engine/Collision/CollisionHandler.h
@@ -1,6 +1,19 @@
 #pragma once
 
-#include "reactphysics3d/reactphysics3d.h"
+#include "Collision.h"
+
+#include <vector>
+#include "glm/glm.hpp"
+
+// Enum for the shapes for the CollisionBodies
+enum class Shape {
+	DRONE,
+	BOX,
+	ARROW
+};
+
+// Forward declerations
+class Entity;
 
 class CollisionHandler
 {
@@ -8,7 +21,30 @@ public:
 	CollisionHandler();
 	virtual ~CollisionHandler();
 
-private:
-	rp3d::CollisionWorld * test;
-};
+	void update(const float& dt);
 
+	// Creates the amount of bodies needed for the level. Done once to have it all after each other in memory.
+	void createCollisionBodies(int num);
+	// Returns an unused collision body, nullptr if noone is available.
+	rp3d::CollisionBody* getUnusedBody();
+	// Add collision body to an entity with a predefined shape
+	void addCollisionToEntity(Entity * entity, Shape shape);
+	// Remove a collision body from the world, this will stop collision checking and destory the pointer
+	void removeCollisionBody(rp3d::CollisionBody * body);
+
+	rp3d::Vector3 toReactVec(const glm::vec3& vec);
+	glm::vec3 toGlmVec(const rp3d::Vector3& vec);
+
+private:
+	rp3d::CollisionWorld * world;
+	rp3d::CollisionBody * player;
+	std::vector<rp3d::CollisionBody*> bodies;
+
+	Collision collision;
+
+	// Shapes
+	rp3d::BoxShape shape = { { 2.0, 2.0, 2.0 } };
+
+	int takenBodies;
+
+};
