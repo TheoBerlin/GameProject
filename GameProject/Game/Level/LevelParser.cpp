c97c44130d50dd74be08b0eb0bee48f055964c05
ADRIAN NORDIN <adno16@student.bth.se>
2019-02-20 14:58:16 +0100

    Changed size on dice

--- GameProject/Game/Level/LevelParser.cpp
+++ GameProject/Game/Level/LevelParser.cpp
@@ -83,7 +83,7 @@ void LevelParser::readEntityBoxes(Level& level)
 		}
 
 		entity->getTransform()->setPosition(position);
-		entity->getTransform()->setScale(0.555f);
+		entity->getTransform()->setScale(0.5f);
 		entity->setModel(model);
 		level.collisionHandler->addCollisionToEntity(entity, SHAPE::BOX);
 	}
