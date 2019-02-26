2ddee85d90294512fc2b1620fee176308c733d09
ADRIAN NORDIN <adno16@student.bth.se>
2019-02-26 13:48:22 +0100

    Fixed merge problem with change of name on class

--- GameProject/Engine/Components/StaticTargetCollision.cpp
+++ GameProject/Engine/Components/StaticTargetCollision.cpp
@@ -28,6 +28,8 @@ void StaticTargetCollision::collide(PlayerCollisionEvent * evnt)
 {
 	if (evnt->entity1 == host || evnt->entity2 == host)
 	{
-		this->host->getTransform()->translate({ -1.0, 0.0, -1.0 });
+		// Change color on collision of drone entity
+		this->host->getModel()->updateInstancingSpecificData(&glm::vec3(1.0, 0.0, 0.0)[0], sizeof(glm::vec3),
+			this->host->getRenderingGroupIndex() * sizeof(glm::vec3), 0, 2);
 	}
 }
