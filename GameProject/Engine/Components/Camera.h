3dbf7c395c1ab099ae7c63b8ef80380224db7202
Anton Åsbrink <anton.asbrink@hotmail.com>
2019-02-28 09:59:22 +0100

    Changed Camera const

--- GameProject/Engine/Components/Camera.h
+++ GameProject/Engine/Components/Camera.h
@@ -18,11 +18,11 @@ public:
 	void update(const float& dt);
 
 	// Returns the cameras up-vector
-	const glm::vec3 getUp();
+	glm::vec3 getUp();
 	// Returns the cameras forward-vector
 	glm::vec3 getForward() const;
 	// Returns the cameras right-vector
-	 const glm::vec3 getRight();
+	glm::vec3 getRight();
 
 	// Returns the View-Projection matrix
 	glm::mat4 getVP() const;
