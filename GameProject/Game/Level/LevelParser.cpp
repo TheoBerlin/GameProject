0a3efb256e4048d4c3687f4bb40cca45ff27da86
ADRIAN NORDIN <adno16@student.bth.se>
2019-03-20 09:45:06 +0100

    Fixed writing directional light to file

--- GameProject/Game/Level/LevelParser.cpp
+++ GameProject/Game/Level/LevelParser.cpp
@@ -364,14 +364,12 @@ void LevelParser::writeLight(Level & level)
 		light[i]["Distance"] = level.lightManager->getPointLights()->at(i)->getDistance();
 	}
 
-	if (!level.lightManager) {
+	if (level.lightManager) {
 		glm::vec4 direction = level.lightManager->getDirectionalLight()->getDirection();
 		glm::vec4 intesity = level.lightManager->getDirectionalLight()->getIntensity();
 	
-
-	jsonFile["DirectionalLight"]["Direction"] = { direction.x, direction.y, direction.z, direction.w };
-	jsonFile["DirectionalLight"]["Intensity"] = { intesity.x, intesity.y, intesity.z, intesity.w };
-
+		jsonFile["DirectionalLight"]["Direction"] = { direction.x, direction.y, direction.z, direction.w };
+		jsonFile["DirectionalLight"]["Intensity"] = { intesity.x, intesity.y, intesity.z, intesity.w };
 	}
 }
 
